c1f04e427e1c82d9dab29979408293305fbd86be
codingpassionate <nirav127@gmail.com>
2025-02-18 11:21:42 -0500

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "gif-gen-tf-state-bucket"
+    bucket  = "gif-generator-tf-state-bucket"
     key     = "ecs/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
